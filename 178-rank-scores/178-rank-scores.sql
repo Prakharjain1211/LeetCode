40810c9884cd829a92938744dd1cb6f1e5224910
Prakharjain1211 <88723250+Prakharjain1211@users.noreply.github.com>
2022-08-11 22:06:52 +0530

    Time: 307 ms (67.22%), Space: 0B (100.00%) - LeetHub

--- 178-rank-scores/178-rank-scores.sql
+++ 178-rank-scores/178-rank-scores.sql
@@ -1,5 +1,3 @@
-SELECT S1.Score,
-(SELECT COUNT(DISTINCT Score) From Scores s2 WHERE s1.Score <= s2.Score)as 'Rank'
-
-FROM Scores S1
-ORDER BY S1.Score DESC;
+SELECT score ,
+Dense_Rank() over(order by score desc) As 'rank'
+From Scores
