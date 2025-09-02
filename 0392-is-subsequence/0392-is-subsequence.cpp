3a88ad544414da50449e67c958b284c3a3248544
Prakhar Jain <88723250+Prakharjain1211@users.noreply.github.com>
2025-09-02 18:49:44 +0530

    Time: 0 ms (100%), Space: 8.6 MB (66.66%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        char ptr = 0;
+       
+
+        for(int i=0;i<t.length();i++)
+        {
+            if(s[ptr] == t[i])
+            {
+                ptr++;
+            }
+        }
+        if(ptr == s.length())
+        return true;
+        return false;
+    }
+};
